79c7a300c24e9caa797a239ae2706026c603e8e7
Luis Mayta <luis@hadenlabs.com>
2022-10-15 13:09:55 -0300

    fix: structure molecule project (#5)

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+def pkg_is_installed(host):
+    package = host.package('td-agent-bit')
+
+    assert package.is_installed
+
+
+def test_service_is_running(host):
+    service = host.service('td-agent-bit')
+
+    assert service.is_running
+    assert service.is_enabled
